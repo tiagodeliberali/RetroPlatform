ffb665b94d24374387373267c5dfc6abd0796fed
Tiago Deliberali <tiago.deliberali@gmail.com>
2018-11-04 22:41:29 -0200

    Added bomb and coins to halloween level

---
+++ Assets/Scripts/PlatformEnemies/SensoredAnimator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace RetroPlatform.PlatformEnemies
+{
+    public class SensoredAnimator : MonoBehaviour
+    {
+        Animator gameObjectAnimator;
+
+        void Awake()
+        {
+            gameObjectAnimator = GetComponent<Animator>();
+        }
+
+        public void DestroyGameObject()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision != null && collision.gameObject.CompareTag("Player"))
+            {
+                gameObjectAnimator.SetBool("PlayerSeen", true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision != null && collision.gameObject.CompareTag("Player"))
+            {
+                gameObjectAnimator.SetBool("PlayerSeen", false);
+            }
+        }
+    }
+}
